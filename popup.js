99e5929d49f0abb7dd13da209831346dd2c9ea3a
jensenq <jensenq@wwu.edu>
2021-10-29 15:50:57 -0700

    toy input from html is collected in js.

--- popup.js
+++ popup.js
@@ -1,22 +1,39 @@
 
+
+
+// when the submit button is clicked, do stuff.
+document.addEventListener('DOMContentLoaded', function() {
+	let input_form = document.getElementById('input_form');
+	input_form.addEventListener('submit', function(e){
+		e.preventDefault();
+		
+		var journal_text = e.target.children.journal_text.value
+		console.log(journal_text)
+		
+		var slider_val = e.target.children.example_slider.value
+		console.log(slider_val)
+		
+    })
+}, false);
+
+
 /* reference code, will not be in our project.
  * gets that HTML element whose ID is "some_cool_text" , and updates
  * the text its currently displaying to whatever value data_from_storage is.
  */
-function update_UI_data_from_storage() {
-  chrome.storage.sync.get("data_from_storage", function(items) {
+function update_UI_from_storage() {
+	chrome.storage.sync.get("data_from_storage", function(items) {
+	if (!chrome.runtime.error) {
+		
+		//document.getElementById("some_cool_text").innerText = items.data_from_storage;
+		console.log("updating UI from storage");
 
-    if (!chrome.runtime.error) {
-      document.getElementById("some_cool_text").innerText = items.data_from_storage;
-    }
-    else {
-      console.log("Runtime error.");
-    }
-  });
+
+	}else{console.log("Runtime error.");}});
 }
 
 document.body.onload = function() {
-	update_UI_data_from_storage();
+	update_UI_from_storage();
 }
 
 /* reference code, will not be in our project.
@@ -26,9 +43,10 @@ document.body.onload = function() {
  * and sets data_from_storage to that. this value is now permanently changed 
  * in storage.
  */
+/*
 document.getElementById("example_set_btn").onclick = function() {
 
-  var text_input = document.getElementById("text_input").value;
+  let text_input = document.getElementById("text_input").value;
   chrome.storage.sync.set({ "data_from_storage" : text_input }, function() {
   update_UI_data_from_storage();
   console.log("set data_from_storage to: ", text_input);
@@ -40,26 +58,5 @@ document.getElementById("example_set_btn").onclick = function() {
   });
   //window.close();
 }
-
-
-// reference code
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-/*
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
 */
+

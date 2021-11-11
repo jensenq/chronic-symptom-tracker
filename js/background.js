94c84b25941f570e6e3d2c66ff8215050409feda
Jaden-Breeland <56038076+Jaden-Breeland@users.noreply.github.com>
2021-11-11 14:24:19 -0800

    Update background.js

--- js/background.js
+++ js/background.js
@@ -2,6 +2,25 @@
  * back-end code
  */
 
+//registering an alarm that chrome will always moniter
+chrome.alarms.create('welcome_alarm', {
+        when: Date.now(),
+        periodInMinutes: 0.1 //notification begins immediately and repeats every 0.1 minutes
+});
+
+
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+        if(alarm.name === 'welcome_alarm'){
+                chrome.notifications.create('welcome', {
+                        type: 'basic',
+                        iconUrl: '/images/get_started128.png',
+                        title: 'Test Notification',
+                        message: 'Welcome to CHronic Symtom Tracker!!'
+                });
+        }
+});
+
 
 
 

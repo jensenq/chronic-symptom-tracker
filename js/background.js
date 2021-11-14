eaf97754a4bf3d49d3041530aa2ccb65a9c965b4
Jaden-Breeland <56038076+Jaden-Breeland@users.noreply.github.com>
2021-11-13 16:15:14 -0800

    modified the notification so it only displays once when you open the extension

--- js/background.js
+++ js/background.js
@@ -1,35 +1,35 @@
 /* background.js
  * back-end code
  */
-
 //registering an alarm that chrome will always moniter
-chrome.alarms.create('welcome_alarm', {
+/*chrome.alarms.create('welcome_alarm', {
         when: Date.now(),
-        periodInMinutes: 0.1 //notification begins immediately and repeats every 0.1 minutes
-});
+	//periodInMinutes: 0.1
+});*/
 
 
 
+/* this creates the same notification but is triggered by the alarm
 chrome.alarms.onAlarm.addListener((alarm) => {
-        if(alarm.name === 'welcome_alarm'){
-                chrome.notifications.create('welcome', {
-                        type: 'basic',
-                        iconUrl: '/images/get_started128.png',
-                        title: 'Test Notification',
-                        message: 'Welcome to CHronic Symtom Tracker!!'
-                });
-        }
-});
-
-
-
-
-
-
-
-
-
-
-
-
+	if(alarm.name === 'welcome_alarm'){
+		chrome.notifications.create('welcome', {
+			type: 'basic',
+			iconUrl: '/images/get_started128.png',
+			title: 'Test Notification',
+			message: 'Welcome to CHronic Symtom Tracker!!'
+		});
+	}
+});*/
+//create notification that shows when you open the extesnion
+chrome.runtime.onMessage.addListener(
+	(request) => {
+		if (request.greeting === "welcome"){
+			chrome.notifications.create('welcome', {
+				type: 'basic',
+				iconUrl: '/images/foxpicture.jpg',
+				title: 'Test Notification',
+				message: 'Welcome to Chronic Symptom Tracker!!'
+			});
+		}
+	});
 
